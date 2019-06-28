4b6ed86566ff4a771ce6e06f21e4b606e7d790ab
Nuno <nuno.nmduarte.us@gmail.com>
2019-06-27 22:10:58 -0700

    added more fucntions to retreive data

--- flaskweb/flask_gdelt.py
+++ flaskweb/flask_gdelt.py
@@ -9,7 +9,17 @@ def webui():
     cameo_codes = PostgresqlObj()
     cameo_categories = cameo_codes.get_all_categories()
     cameo_subcategories = cameo_codes.get_all_subcategories()
+    countries = cameo_codes.get_countries_with_most_events()
+    tone_per_month = cameo_codes.avg_tone_per_month()
+    #types_across_time = cameo_codes.types_across_time()
+    top_channels = cameo_codes.top_channels()
+    top_words = cameo_codes.top_words()
+    #print(top_channels)
     cameo_codes.close()
 
-    data = {'categories': cameo_categories, 'subcategories': cameo_subcategories}
+    data = {'categories': cameo_categories, 'subcategories': cameo_subcategories, 'countries': countries, 'tone_per_month' : tone_per_month, 'top_channels' : top_channels, 'top_words': top_words }
+    #print(types_across_time)
     return render_template('index.html', title='GDELT+', data=data)
+
+if __name__ == "__main__":
+    app.run(port="80", host="0.0.0.0",debug=True)
