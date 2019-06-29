c544b229b1c30174d0dcd9b6d8c582cd21637c15
Nuno <nuno.nmduarte.us@gmail.com>
2019-06-28 22:51:44 -0700

    updated code to deal with the function to query the data

--- flaskweb/flask_gdelt.py
+++ flaskweb/flask_gdelt.py
@@ -1,25 +1,29 @@
+import queries
 from flask import Flask, render_template
 from postgres_data import PostgresqlObj
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 def webui():
-    cameo_codes = PostgresqlObj()
-    cameo_categories = cameo_codes.get_all_categories()
-    cameo_subcategories = cameo_codes.get_all_subcategories()
-    countries = cameo_codes.get_countries_with_most_events()
-    tone_per_month = cameo_codes.avg_tone_per_month()
-    #types_across_time = cameo_codes.types_across_time()
-    top_channels = cameo_codes.top_channels()
-    top_words = cameo_codes.top_words()
-    #print(top_channels)
-    cameo_codes.close()
+    db_data = PostgresqlObj()
 
-    data = {'categories': cameo_categories, 'subcategories': cameo_subcategories, 'countries': countries, 'tone_per_month' : tone_per_month, 'top_channels' : top_channels, 'top_words': top_words }
-    #print(types_across_time)
+    cameo_categories = db_data.execQuery(queries.get_all_categories)
+    cameo_subcategories = db_data.execQuery(queries.get_all_subcategories)
+    countries = db_data.execQuery(queries.get_countries_with_most_events).toJson()
+    tone_per_month = db_data.execQuery(queries.avg_tone_per_month)
+    top_channels = db_data.execQuery(queries.top_channels)
+    top_words = db_data.execQuery(queries.top_words)
+    db_data.close()
+
+    # data to be passed to front end
+    data = {'categories': cameo_categories, 'subcategories': cameo_subcategories, 'countries': countries, 'tone_per_month': tone_per_month, 'top_channels': top_channels, 'top_words': top_words}
+
+    #return top_words
     return render_template('index.html', title='GDELT+', data=data)
 
+
 if __name__ == "__main__":
-    app.run(port="80", host="0.0.0.0",debug=True)
+    app.run(port="80", host="0.0.0.0", debug=True)
